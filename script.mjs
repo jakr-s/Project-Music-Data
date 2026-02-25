3cc1c99e7c0e082b1c54a6051a7a09345f2db946
Jak R-S <176810031+jakr-s@users.noreply.github.com>
2026-02-25 15:30:01 +0000

    feat: implement user dropdown population on page load

--- script.mjs
+++ script.mjs
@@ -1,11 +1,23 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
+import { getUserIDs } from "./data.mjs";
 
-import { countUsers } from "./common.mjs";
+function populateUserDropdown() {
+  const select = document.getElementById("user-select");
 
-window.onload = function () {
-  document.querySelector("body").innerText = `There are ${countUsers()} users`;
-};
+  const defaultOption = document.createElement("option");
+  defaultOption.value = "";
+  defaultOption.textContent = "-- Select a user --";
+  select.appendChild(defaultOption);
+
+  for (const id of getUserIDs()) {
+    const option = document.createElement("option");
+    option.value = id;
+    option.textContent = `User ${id}`;
+    select.appendChild(option);
+  }
+
+  select.addEventListener("change", () => {
+    displayUserData(select.value);
+  });
+}
+
+window.addEventListener("DOMContentLoaded", populateUserDropdown);
